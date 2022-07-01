a4c269dbb2d173c802fe24cf51ec3f59e598c906
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-07-01 16:42:50 -0300

    Initial commit

---
+++ src/main/java/br/com/handora/springoauth2test/SpringOauth2TestApplication.java
@@ -0,0 +1,13 @@
+package br.com.handora.springoauth2test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/handora/springoauth2test/SpringOauth2TestApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.handora.springoauth2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2TestApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
